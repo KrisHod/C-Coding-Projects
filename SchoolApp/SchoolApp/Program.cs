d1c44b0b5fc6830362e5dad485adaab96a6bd406
Kris <krisgodom@gmail.com>
2022-01-18 15:39:49 -0400

    add basic Entity Framework Code-First app

---
+++ SchoolApp/SchoolApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SchoolApp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Sasha" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+                Console.WriteLine("Student {0} was added", stud.StudentName);
+                Console.ReadLine();
+            }
+        }
+    }
+}
